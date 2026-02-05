01197a800f12ba267f8712fad91a852000438a4c
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2026-02-05 17:35:16 -0500

    Refactor error component layout and fix client data access. Updated error message styling and adjusted client data handling to directly access the client array.

--- app/clients/error.tsx
+++ app/clients/error.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 export default function Error() {
-    return <>
+    return <div className="flex flex-col justify-center items-center gap-8 h-screen font-extrabold text-xl">
         <h1>Algo ha salido mal al cargar los clientes</h1>
-        <div className="text-4xl text-center animate-pulse">🦧</div>
-    </>
+        <div className="text-8xl text-center animate-pulse">🦧</div>
+    </div>
 }
--- components/clients/clientName.tsx
+++ components/clients/clientName.tsx
@@ -7,11 +7,11 @@ import { use } from "react"
 export default function Client({clientPromise} : {clientPromise: Promise<any>}) {
     const client = use(clientPromise)
 
-    if (client.clients.length === 0) {
+    if (client.length === 0) {
         return <div>No se encontró el cliente</div>
     }
 
-    const first = client.clients[0]
+    const first = client[0]
     return (
         <>
             <h1>Cliente encontrado</h1>
