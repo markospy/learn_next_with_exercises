87bb2f418234c49653595b29cc7af7536c8c528d
Marcos Antonio Avila Morales <mis.redes.work@gmail.com>
2026-02-05 17:27:17 -0500

    Enhance client search and API error handling. Added validation for search input in handleSearch and improved getClients and getClientByName functions to handle errors gracefully.

--- app/actions/clients/handleSearch.ts
+++ app/actions/clients/handleSearch.ts
@@ -3,5 +3,8 @@ import { redirect } from "next/navigation"
 
 export const handleSearch = async (formData: FormData) => {
     const search = formData.get('search') as string
-    redirect(`/clients/${encodeURIComponent(search.trim())}`)
+    if (search?.trim()) {
+        redirect(`/clients/${encodeURIComponent(search.trim())}`)
+    }
+    return
 }
--- app/utils/api/clients/get.ts
+++ app/utils/api/clients/get.ts
@@ -1,18 +1,18 @@
 const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ7XCJpZF91c2VyXCI6IFwiNzY4NTRlYTYtMmZmZS00MzhkLWFlNjgtNjFiZDRlMjY1MjljXCIsIFwiaWRfZ3ltXCI6IFwiYWNjOTE3YzQtMWE3NC00YjExLTk0YzItZTA1ZGYxMDY1MzcwXCIsIFwidXNlcm5hbWVcIjogXCJhZHJpYW5hMVwifSIsInNjb3BlcyI6WyJneW1fc3VwZXJfYWRtaW4iXSwiZXhwIjoxNzcwNjkzNDUxfQ.oiyacBTI-zivtCSCb3YcUo80lX0u5NWttA4dTLT9rAY';
 
-export const getClients = async (name: string = '') => {
+export const getClients = async () => {
     let headers = new Headers()
     headers.append('accept', 'application/json')
     headers.append('Authorization', token)
 
 
-    let init = { method: 'GET',
-                headers: headers,
-            }
-
-    const clients = await fetch(`https://fitlink.info/v1/clients/?page=0&size_page=10&to_excel=false&name=${name}&critery=id_zkteco&order=desc`, init)
-
-    return clients.json()
+    try {
+        const clients = await fetch('https://fitlink.info/v1/clients/?page=0&size_page=10&to_excel=false&critery=id_zkteco&order=desc', { method: 'GET', headers })
+        return await clients.json()
+    } catch (error) {
+        console.error(error)
+        throw new Error('Error al obtener los clientes')
+    }
 }
 
 
@@ -21,12 +21,11 @@ export const getClientByName = async (name: string = '') => {
     headers.append('accept', 'application/json')
     headers.append('Authorization', token)
 
-
-    let init = { method: 'GET',
-                headers: headers,
-            }
-
-    const clients = await fetch(`https://fitlink.info/v1/clients/name/${name}`, init)
-
-    return clients.json()
+    try {
+        const clients = await fetch(`https://fitlink.info/v1/clients/name/${encodeURIComponent(name)}`, { method: 'GET', headers })
+        return await clients.json()
+    } catch (error) {
+        console.error(error)
+        throw new Error('Error al obtener los clientes')
+    }
 }
